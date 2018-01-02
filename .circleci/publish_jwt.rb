8cc4a00dffac0399c5fdda52126204c7d7afd393
Shunsuke TAKENO <shunsuke.takeno@retty.me>
2018-01-03 07:29:31 +0900

    Add circleci

---
+++ .circleci/publish_jwt.rb
@@ -0,0 +1,21 @@
+#!/usr/bin/env ruby
+
+require 'openssl'
+require 'jwt'  # https://rubygems.org/gems/jwt
+
+# Private key contents
+private_pem = File.read(YOUR_PATH_TO_PEM)
+private_key = OpenSSL::PKey::RSA.new(private_pem)
+
+# Generate the JWT
+payload = {
+  # issued at time
+  iat: Time.now.to_i,
+  # JWT expiration time (10 minute maximum)
+  exp: Time.now.to_i + (10 * 60),
+  # GitHub App's identifier
+  iss: YOUR_ISSUE_NUMBER
+}
+
+jwt = JWT.encode(payload, private_key, "RS256")
+puts jwt
